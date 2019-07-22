405f0a492bde3e90bc3e6376301c4c10cce78fb7
Maia Boyd <maiajanae2001@gmail.com>
2019-07-22 14:33:37 -0500

    fixed Home Page

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from google.appengine.ext import ndb
 import jinja2
 import webapp2
 
+from google.appengine.api import users
+
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -14,7 +16,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class HomePage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        template = JINJA_ENVIRONMENT.get_template('templates/main.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/homepage.html')
         data = {
           'user': user,
           'login_url': users.create_login_url('/'),
