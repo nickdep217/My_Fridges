9725550b9ed568695123fbb31ba3ce61075c0903
Sydney Perez <sydney.perez9130@gmail.com>
2019-07-22 12:23:37 -0700

    Added jinga and appengine so login can work

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import webapp2
+import jinja2
+import os
 
+from google.appengine.api import users
 
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 
 class HomePage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        template = JINJA_ENVIRONMENT.get_template('templates/main.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/homepage.html')
         data = {
           'user': user,
           'login_url': users.create_login_url('/'),
