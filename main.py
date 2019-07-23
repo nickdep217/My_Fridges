941ac85e166cba61e58e7e9585ef8079280e3fdc
Maia Boyd <maiajanae2001@gmail.com>
2019-07-23 16:28:53 -0500

    adding user stuff

--- main.py
+++ main.py
@@ -22,11 +22,15 @@ def root_parent():
 class Meat(ndb.Model):
 
     name = ndb.StringProperty()
-
+    user = ndb.UserProperty()
 class Fruit(ndb.Model):
 
     name = ndb.StringProperty()
+    user = ndb.UserProperty()
+class Pantry(ndb.Model):
 
+    name = ndb.StringProperty()
+    user = ndb.UserProperty()
 class HomePage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
@@ -59,14 +63,16 @@ class FridgePage(webapp2.RequestHandler):
           'user': user,
           'login_url': users.create_login_url('/'),
           'logout_url': users.create_logout_url(self.request.uri),
-            'meats': Meat.query(ancestor=root_parent()).fetch(),
-            'fruits': Fruit.query(ancestor=root_parent()).fetch(),
+            'meats': Meat.query(Meat.user==user,ancestor=root_parent()).fetch(),
+            'fruits': Fruit.query(Fruit.user==user,ancestor=root_parent()).fetch(),
+            'pantries': Pantry.query(Pantry.user==user,ancestor=root_parent()).fetch(),
         }
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write(template.render(data))
     def post(self):
         each_meat = Meat(parent=root_parent())
         each_meat.name = self.request.get('meat_name')
+        each_meat.user = users.get_current_user()
 
         each_meat.put()
             # redirect to '/' so that the get() version of this handler will run
@@ -98,18 +104,22 @@ class RecipePage(webapp2.RequestHandler):
         self.response.write(template.render(data))
 
 class NewFruits(webapp2.RequestHandler):
+
     def post(self):
         new_fruit = Fruit(parent=root_parent())
         new_fruit.name = self.request.get('fruit_name')
         new_fruit.put()
+        new_fruit.user = users.get_current_user()
         # redirect to '/' so that the get() version of this handler will run
         # and show the list of dogs.
         self.redirect('/fridge')
 class NewPantries(webapp2.RequestHandler):
+
     def post(self):
-        new_pantry = Fruit(parent=root_parent())
+        new_pantry = Pantry(parent=root_parent())
         new_pantry.name = self.request.get('pantry_name')
         new_pantry.put()
+        new_pantry.user = users.get_current_user()
         # redirect to '/' so that the get() version of this handler will run
         # and show the list of dogs.
         self.redirect('/fridge')
