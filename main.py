5e8184e61ddf6e7a963a8b5bf53bd13d38489ef0
Maia Boyd <maiajanae2001@gmail.com>
2019-07-23 10:03:34 -0500

    made all pages able to login

--- main.py
+++ main.py
@@ -27,27 +27,51 @@ class HomePage(webapp2.RequestHandler):
 
 class AboutPage(webapp2.RequestHandler):
     def get(self):
+        user = users.get_current_user()
         template = JINJA_ENVIRONMENT.get_template('templates/aboutpage.html')
+        data = {
+          'user': user,
+          'login_url': users.create_login_url('/'),
+          'logout_url': users.create_logout_url(self.request.uri),
+        }
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(template.render())
+        self.response.write(template.render(data))
 
 class FridgePage(webapp2.RequestHandler):
     def get(self):
+        user = users.get_current_user()
         template = JINJA_ENVIRONMENT.get_template('templates/fridgepage.html')
+        data = {
+          'user': user,
+          'login_url': users.create_login_url('/'),
+          'logout_url': users.create_logout_url(self.request.uri),
+        }
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(template.render())
+        self.response.write(template.render(data))
 
 class ShoppingListPage(webapp2.RequestHandler):
     def get(self):
+        user = users.get_current_user()
         template = JINJA_ENVIRONMENT.get_template('templates/shopping_listpage.html')
+        data = {
+          'user': user,
+          'login_url': users.create_login_url('/'),
+          'logout_url': users.create_logout_url(self.request.uri),
+        }
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(template.render())
+        self.response.write(template.render(data))
 
 class RecipePage(webapp2.RequestHandler):
     def get(self):
+        user = users.get_current_user()
         template = JINJA_ENVIRONMENT.get_template('templates/recipepage.html')
+        data = {
+          'user': user,
+          'login_url': users.create_login_url('/'),
+          'logout_url': users.create_logout_url(self.request.uri),
+        }
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(template.render())
+        self.response.write(template.render(data))
 
 app = webapp2.WSGIApplication([
     ('/', HomePage),
